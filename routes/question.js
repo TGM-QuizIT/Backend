ced80bab2867a34650b1c77ac2b59e912a00dbb6
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2024-12-01 16:49:54 +0100

    wrote request for adding a question

--- routes/question.js
+++ routes/question.js
@@ -1,21 +1,93 @@
 var express = require('express');
 var router = express.Router();
-const { executeQuery } = require("../config/database");
+const {executeQuery} = require("../config/database");
 const {
     validateBody,
     validateQuery,
     validateKey
 } = require("../config/validator");
-const { createErrorResponse, createSuccessResponse } = require("../config/response");
+const {createErrorResponse, createSuccessResponse} = require("../config/response");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Question' });
+/* Fach hinzufügen */
+router.post('/', function (req, res) {
+    if (!validateKey(req, res)) {
+        return;
+    }
+    const data = req.body;
+    const expected = {
+        questionText: 'string',
+        options: [
+            {
+                optionText: 'string',
+                optionCorrect: 'boolean',
+            },
+        ],
+        focusId: 'number',
+        mChoice: 'boolean',
+        textInput: 'optional boolean',
+        imageAddress: 'optional string'
+    };
+    if (validateBody(data, expected, res)) {
+        return;
+    }
+
+    executeQuery("CALL InsertQuestion(?,?,?,?,?)", [data.questionText, data.focusId, data.mChoice, data.textInput, data.imageAddress], res,
+        (result) => {
+            if (result[0][0].result == "404") {
+                res.status(404).json(createErrorResponse("Focus not found"));
+            } else {
+                const questionId = result[0][0].id;
+                const options = data.options;
+                options.forEach((option) => {
+                    executeQuery("CALL InsertOption(?,?,?)", [option.optionText, option.optionCorrect, questionId], res,
+                        (otherResult) => {
+                            if (otherResult[0][0].result == "404") {
+                                res.status(404).json(createErrorResponse("Question not found"));
+                            }
+                        },
+                        (error) => {
+                            console.error(error)
+                            res.status(500).json(createErrorResponse('Internal Server Error'));
+                        }
+                    );
+                });
+                executeQuery("CALL FetchQuestion(?)", [questionId], res,
+                    (otherResult) => {
+                        const questionData = otherResult[0];
+                        const options = questionData.map(option => ({
+                            optionId: option.optionId,
+                            optionText: option.optionText,
+                            optionCorrect: option.optionCorrect === 1
+                        }));
+                        const question = {
+                            questionId: questionId,
+                            questionText: questionData[0].questionText,
+                            options: options,
+                            focusId: questionData[0].focusId,
+                            mChoice: questionData[0].mChoice === 1,
+                            textInput: questionData[0].textInput === 1,
+                            imageAddress: questionData[0].imageAddress
+                        };
+                        res.status(200).json(createSuccessResponse({question: question}));
+                    },
+                    (error) => {
+                        console.error(error)
+                        res.status(500).json(createErrorResponse('Internal Server Error'));
+                    }
+                );
+            }
+        },
+        (error) => {
+            console.error(error)
+            res.status(500).json(createErrorResponse('Internal Server Error'));
+        }
+    );
+
 });
 
 /* Fach löschen */
-router.delete('/', function(req, res) {
-    if (!validateKey(req,res)) {
+router.delete('/', function (req, res) {
+    if (!validateKey(req, res)) {
         return;
     }
     const data = req.query;
@@ -23,7 +95,7 @@ router.delete('/', function(req, res) {
         id: 'number',
     };
 
-    if(validateQuery(data, expected, res)) {
+    if (validateQuery(data, expected, res)) {
         return;
     }
 
