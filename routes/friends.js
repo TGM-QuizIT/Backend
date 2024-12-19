e97c31c28faa788166bb9ec53580436c1fe70374
Raphael Tarnoczi <raphi.tarnoczi@gmail.com>
2024-12-19 15:00:35 +0100

    wrote request to delete friendships

--- routes/friends.js
+++ routes/friends.js
@@ -8,7 +8,7 @@ const {
 } = require("../config/validator");
 const {createErrorResponse, createSuccessResponse} = require("../config/response");
 
-/* User hinzufügen */
+/* Freundschaft hinzufügen */
 router.post('/', function (req, res) {
     if (!validateKey(req, res)) {
         return;
@@ -36,6 +36,34 @@ router.post('/', function (req, res) {
         (error) => {
             console.error(error)
             res.status(500).json(createErrorResponse('Internal Server Error'));
+        }
+    );
+});
+
+/* Freundschaft löschen */
+router.delete('/', function (req, res) {
+    if (!validateKey(req, res)) {
+        return;
+    }
+    const data = req.query;
+    const expected = {
+        id: 'number',
+    };
+
+    if (validateQuery(data, expected, res)) {
+        return;
+    }
+
+    executeQuery("CALL DeleteFriendship(?)", [data.id], res,
+        (result) => {
+            if (result[0][0] && result[0][0].result == "404") {
+                res.status(404).json(createErrorResponse("Friendship not found"));
+            } else {
+                res.status(200).json(createSuccessResponse());
+            }
+        },
+        (error) => {
+            res.status(500).json(createErrorResponse("Internal Server Error"));
         }
     );
 });
